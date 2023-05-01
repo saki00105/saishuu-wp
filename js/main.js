f854c322c188442f0acf7452d90122660b675cce
saki00105 <126153940+saki00105@users.noreply.github.com>
2023-05-02 00:01:35 +0900

    add-c

--- js/main.js
+++ js/main.js
@@ -1,33 +1,58 @@
 
-// $(function(){
-//     //サイドバーのスライド表示
-//     $(".l-sideber__box").on('click', function(){
-//         $(this).addClass("is-open");
-//         $(".l-sideber").addClass("is-open");
-//         // return false;
-//     })
 
-//     $(".p-sideber__box").on('click', function(){
-//         $(".l-sideber__box").removeClass("is-open");
-//         $(".l-sideber").removeClass("is-open");
+$(function(){
+    $(".l-sideber__box").on('click', function(){
+        $(this).addClass("is-open");
+        $(".l-sideber").addClass("is-open");
+        return false;
+    });
 
-//         // return false;
-//     })
+    $(".p-sideber__box").on('click', function(){
+        $(".l-sideber__box").removeClass("is-open");
+        $(".l-sideber").removeClass("is-open");
+        return false;
+    });
 
+});
 
-    
+console.log("テスト");
 
-
-// });
 
 //暗転
-$(function() {
-    $(".l-sideber__box").click(function() {
-        $("l-sideber__layer").fadeIn();
-    });
+// $(function() {
+//     $(".l-sideber__box").click(function() {
+//         $("l-sideber__layer").fadeIn();
+//     });
 
-    $(".p-sideber__box").click(function() {
-        $("l-sideber__layer").fadeOut();
-    });
-    return false;
-});
+//     $(".p-sideber__box").click(function() {
+//         $("l-sideber__layer").fadeOut();
+//     });
+//     return false;
+// });
+
+
+// $(function() {
+//     // ハンバーガーメニューをクリックしたときの処理
+//     $('.l-sideber__box').on('click', function() {
+//         $(this).addClass("is-open");
+
+//         console.log("test");
+
+//       // オーバーレイをフェードインして表示する
+//       $('.l-sideber__layer').fadeIn().addClass('is-open');
+//       // サイドバーを表示する
+//       $('.l-sideber').addClass('is-open');
+//       return false;
+//     });
+    
+//     // オーバーレイをクリックしたときの処理
+//     $('.p-sideber__box').on('click', function() {
+//       // オーバーレイを非表示にする
+//       $('.l-sideber__layer').fadeOut().removeClass('is-open');
+//       // サイドバーを非表示にする
+//       $('.l-sideber__box').removeClass('is-open');
+//       $(".l-sideber").removeClass("is-open");
+//       return false;
+//     });
+// });
+  
