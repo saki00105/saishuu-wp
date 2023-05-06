519be3135780cd0b2277e85536e02d5767c142bc
saki00105 <126153940+saki00105@users.noreply.github.com>
2023-05-06 15:34:10 +0900

    dequeue

--- js/main.js
+++ js/main.js
@@ -2,27 +2,23 @@
 
 $(function() {
     // サイドバーのクリック表示
-    // $('.l-sideber__box').on('click', function() {
+    $('.l-sideber__box').on('click', function() {
 
-    $(document).on('click', '.l-sideber__box', function(){
         $(this).addClass("is-open");
-        $('.l-sideber__layer').fadeIn().addClass('is-open');
-        $(".l-sideber").delay(300).queue(function(){
-            $(this).addClass("is-open");
+        $('.l-sideber__layer').fadeIn(500).addClass("is-open");
+        $(".l-sideber").delay(500).queue(function(){
+            $(this).addClass("is-open").dequeue();
         });
         return false;
     });
-    console.log("テスト1");
 
 
     //サイドバーを閉じる
-    // $('.p-sideber__box').on('click', function() {
-    $(document).on('click', '.p-sideber__box', function() {
+    $('.p-sideber__box').on('click', function() {
 
       $('.l-sideber__layer').hide().removeClass('is-open');
       $('.l-sideber__box').removeClass('is-open');
       $(".l-sideber").removeClass("is-open");
       return false;
     });
-console.log("テスト２");
 });
