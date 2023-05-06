5058cee5092fca6113959988f22845388d288a05
saki00105 <126153940+saki00105@users.noreply.github.com>
2023-05-06 18:19:24 +0900

    add-dequeue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,7 @@
 
-
 $(function() {
     // サイドバーのクリック表示
     $('.l-sideber__box').on('click', function() {
-
         $(this).addClass("is-open");
         $('.l-sideber__layer').fadeIn(500).addClass("is-open");
         $(".l-sideber").delay(500).queue(function(){
@@ -12,13 +10,13 @@ $(function() {
         return false;
     });
 
-
     //サイドバーを閉じる
     $('.p-sideber__box').on('click', function() {
-
       $('.l-sideber__layer').hide().removeClass('is-open');
       $('.l-sideber__box').removeClass('is-open');
       $(".l-sideber").removeClass("is-open");
       return false;
     });
 });
+
+
