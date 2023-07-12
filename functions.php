e0013cb0eed62fcbc757cadb2ea7726a4363e951
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-07-12 21:45:06 +0900

    wp_menu

--- functions.php
+++ functions.php
@@ -1,10 +1,6 @@
 <?php
     add_theme_support( 'menus' );
     add_theme_support( 'title-tag');
-    register_nav_menus( array(
-        'sideber-menu' => esc_html__( 'Sideber Menu', 'rtbread' ) ,
-        'footer-menu'  => esc_html__( 'Footer Menu', 'rtbread' ),
-    ) );
     
    
     //タイトル出力
@@ -24,10 +20,10 @@
         wp_enqueue_style( 'roboto', 'https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap', array() );
         // wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css', array(), '4.7.0' );
        
-        wp_enqueue_style( 'reset', get_template_directory_uri() . '/css/reset.css', array(), '4.5.0' );
-        wp_enqueue_style( 'css-style', get_template_directory_uri() . '/css/style.css', array(), '1.0.0' );
-        wp_enqueue_style( 'app', get_template_directory_uri() . '/css/app.css', array(), '1.0.0' );
-        wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css', array(), '1.0.0' );
+        wp_enqueue_style( 'reset', get_theme_file_uri( '/css/reset.css' ), array(), '1.0.0' );
+        wp_enqueue_style( 'css-style', get_theme_file_uri('/css/style.css')  , array(), '1.0.0' );
+        wp_enqueue_style( 'app', get_theme_file_uri('/css/app.css')  , array(), '1.0.0' );
+        wp_enqueue_style( 'style', get_theme_file_uri('/style.css')  , array(), '1.0.0' );
         // wp_enqueue_script( 'js', get_stylesheet_directory_uri() . 'https://code.jquery.com/jquery-3.6.4.min.js', array( 'js' ) );
         // wp_enqueue_script( 'main', get_stylesheet_directory_uri() . '/js/main.js', array( 'main' ) );
     
@@ -51,8 +47,40 @@
     // }
     // add_action( 'widgets_init', 'wpbeg_widgets_init' );
 
-    //カスタムメニュー
-    // function  register_nav_menus(){
-        
-    // }   
-    // add_action( 'after_setup_theme', 'register_my_menus' );
+    //カスタムメニュー 
+	// wp_nav_menu( array(
+	// 	'menu'            => '',
+	// 	'menu_class'      => 'menu-item',
+	// 	'menu_id'         => '{メニューのスラッグ}-{連番}',
+	// 	'container'       => 'div',
+	// 	'container_class' => 'menu-{メニューのスラッグ}-container',
+	// 	'container_id'    => '',
+	// 	'fallback_cb'     => 'wp_page_menu',
+	// 	'before'          => '',
+	// 	'after'           => '',
+	// 	'link_before'     => '',
+	// 	'link_after'      => '',
+	// 	'echo'            => true,
+	// 	'depth'           => 0,
+	// 	'walker'          => '',
+	// 	'theme_location'  => '',
+	// 	'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+	// ));
+    
+    // function register_custom_menu() {
+    //     register_nav_menus( array(
+    //         'sideber-menu' => esc_html__( 'Sideber Menu', 'rtbread' ) ,
+    //         'footer-menu'  => esc_html__( 'Footer Menu', 'rtbread' ),
+    //     ) );
+
+    // }
+    // add_action('init', 'register_custom_menu');
+
+    function register_custom_menu() {
+        register_nav_menus( array(
+            'sideber-menu' => 'Sideber Menu', 'rtbread',
+            'footer-menu'  => 'Footer Menu', 'rtbread' ,
+        ) );
+
+    }
+    add_action('after_setup_theme', 'register_custom_menu');
