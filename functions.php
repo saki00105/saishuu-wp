99ea4873bea4587388ef424a1861b170d6f520f3
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-07-22 17:56:07 +0900

    single

--- functions.php
+++ functions.php
@@ -3,18 +3,8 @@
 add_theme_support( 'title-tag' );
 //本体ギャラリーCSS停止
 add_filter( 'use_default_gallery_style', '__return_false' );
+//サムネイル機能
 add_theme_support('post-thumbnails');
-
-//タイトル出力
-// function wpbeg_title( $title ) {
-//     if ( is_front_page() && is_home() ) { //トップページなら
-//         $title = get_bloginfo( 'name', 'display' );
-//     } elseif ( is_singular() ) { //シングルページなら
-//         $title = single_post_title( '', false );
-//     }
-//         return $title;
-//     }
-// add_filter( 'pre_get_document_title', 'wpbeg_title' );
 
 
 //スタイルシート
@@ -45,10 +35,10 @@ add_action( 'after_setup_theme', 'theme_setup');
 
 //サムネイルの表示
 function thumb_url( $size ) {
-  if( has_post_thumbnail() ) {//サムネイルがある場合
+  if( has_post_thumbnail() ) {
     $postthumb =wp_get_attachment_image_src( get_post_thumbnail_id(), $size );
     $url = $postthumb[0];
-    } else {//サムネイルがない場合
+    } else {
     $url = get_template_directory_uri() . '/img/20200502_noimage';
   }
   return $url;
--- page.php
+++ page.php
@@ -1,13 +1,19 @@
 
 <?php get_header(); ?>     
     <main class="l-main">
-        <div class="p-visual--page" style="background-image:url
-                 (<?php echo thumb_url( 'full' ); ?>)">
+        <?php
+            if( has_post_thumbnail() ) {
+                $postthumb =wp_get_attachment_image_src( get_post_thumbnail_id(),'full' );
+                $url = $postthumb[0];
+            }
+        ?>
+        <div class="p-visual--page" style="background-image: url(<?php echo $url; ?>);"></div>
 
-            <h1 class="p-visual--single_page-title c-text--h1 c-color--white c-fw-700 u-position">ショップについて</h1>
+
+        <h1 class="p-visual--single_page-title c-text--h1 c-color--white c-fw-700 u-position"><?php the_title( );?></h1>
      
-        </div>
-        
+    
+
         <div class="l-inner--single_page">
             <?php the_content( ); ?>
         </div>
