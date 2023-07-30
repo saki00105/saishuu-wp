d198171b1f7609cd68a412dcd001a3b605744820
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-07-31 00:08:45 +0900

    seach

--- search.php
+++ search.php
@@ -1,54 +1,67 @@
 <?php get_header(); ?>
 
-        <main class="l-main">
-            <div class="p-visual--archive c-color_bg--black-top">
-                <h1 class="p-visual--archive-title c-text--h1 c-color--white c-fw-700">Seach:</h1>
-            </div>
+<main class="l-main">
+        <div class="p-visual--archive c-color_bg--black-top">
+            <img class="p-visual--archive " src="<?php echo get_stylesheet_directory_uri(); ?>/img/page-title_arcive.jpg" alt="archiveトップのハンバーガーの写真">
+            <h1 class=" p-visual--archive-title c-text--h1 c-color--white c-fw-700 c-color_bg--black-top">Seach:
+                <span><?php the_search_query(); ?></span>
+            </h1>
+        </div>
+        
+        <div class="l-inner--archive">
+        
+            <h2 class="c-text--h2"><?php single_cat_title(); ?></h2>
+            <article class="p-archive__text c-text--p">
+                <?php echo category_description(); ?>
+            </article>
 
-            <div class="l-inner--archive">
-                <h2 class="c-text--h2"></h2>
-                <p class="p-archive__text c-text--p"></p>
-
-                
-
-
-            </div>
-
+            
             <ul class="p-card__archive">
-                <?php
-                    if( have_posts() ) : //1.投稿データがあるかの条件分岐。
-                        while( have_posts() ) : // 2.表示する投稿データがあれば繰り返し処理開始
-                            the_post(); //3.ループ処理に必要なカウント処理等
-                            ?>
-                                    <li>
-                                        <figure class="p-card__archive-wrap c-flex">
-                                            <div class="p-card__archive-img c-flex">
-                                                <?php 
-                                                    if (has_post_thumbnail( )):
-                                                        the_post_thumbnail( );
-                                                    else:  
-                                                        ?><div class="no-thumbnail"></div><?php
-                                                    endif;
-                                                ?>
-                                            </div>
-                                            <figcaption class="p-card__archive-content">
-                                                <h2 class="c-text--h2 c-color--white">見出しが入ります</h2>
-                                                <h3 class="p-card__archive-sub c-color--white">小見出しが入ります</h3>
-                                                <p class="p-card__archive-text c-text--p c-color--white">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p> 
-                                                <a  class="p-card__archive-button c-button--archive c-fw-700" href="/">詳しく見る</a>
-                                            </figcaption>
-                                        </figure>
-                                    </li>
+                <?php 
+                    if (have_posts(  )):
+                        while(have_posts(  )):
+                            the_post(  ); ?> 
                             
-                        endwhile; // 5.繰り返し処理ここまで。投稿データがまだあればwhileに戻る。なければ終了
-                    else : //6.投稿データがなければ
-                        ?><p>表示する記事がありません</p><?php //7.ない時の処理
-                    endif; ?> //8.条件分岐終了
-                ?>
-            </ul>  
+                            <li  id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                                <figure class="p-card__archive-wrap c-flex">
+                                    <div class="p-card__archive-img c-flex">
+                                        <?php the_post_thumbnail(  );?>
+                                    </div>
+                                    <figcaption class="p-card__archive-content c-color--white">
+                                        <h2 class="c-text--h2"><?php the_title();?></h2>
+                                        
+                                        <?php     
+                                            if (has_excerpt()) {
+                                                echo '<div class="p-card__archive-text c-text--p">';
+                                                the_excerpt();
+                                                echo '</div>';
+                                               
+                                            } 
+                                            else{
+                                                echo '<div class="p-card__archive-text c-text--p">';
+                                                echo wp_trim_words(get_the_content(), 50, '' );
+                                                echo '</div>';
+                                            }
+                                        ?>
+                                        
+                                        <a  class="p-card__archive-button c-button--archive c-fw-700" href="<?php the_permalink( ); ?>">詳しく見る</a>
+                                    </figcaption>
+                                </figure>
+                            </li>
+                        <?php endwhile;?>
+                    <?php else:?>
+                        <p>記事はありません</p>
+                <?php endif; ?>
+            </ul>
 
+            <ul class="p-pagination c-flex c-center">
+                <li class="c-flex ">
+                    <?php wp_pagenavi(); ?>
+                </li>    
+            </ul>
+        </div>
+    </main>
 
-        </main>
 
 
 <?php get_sidebar(); ?>
--- searchform.php
+++ searchform.php
@@ -4,7 +4,9 @@
 </form> -->
 
 
-<form class="p-header c-fw-700" action="<?php echo home_url('/');?>, $scheme:string|null )" method="get">
-    <input type="search" name="s" id="s" placeholder="" class="p-header__search c-border--form">
+<form class="p-header c-fw-700" action="<?php echo home_url('/');?>" method="get">
+    <input type="search" name="s" id="s" placeholder="" value="<?php the_search_query(); ?>"class="p-header__search c-border--form">
+    <img class="p-header__icon " src="<?php echo get_stylesheet_directory_uri(); ?>/img/mushimegane.png" alt="検索虫眼鏡の写真">
     <input type="submit" id="searchsubmit" value="検索" class="p-header__submit c-button--form c-border--form">
 </form>
+
