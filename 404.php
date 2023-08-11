bf717f70bc532fc2b2c44e31a22919f00fd50a8a
githubアカウントのユーザー名 <126153940+saki00105@users.noreply.github.com>
2023-08-11 23:06:53 +0900

    404-add

--- 404.php
+++ 404.php
@@ -8,7 +8,9 @@
     <main class="l-main">
         <div class="p-404__content">
             <p class="c-text--h3">アクセスしたページは見つかりませんでした。</p><br>
-            <p class="c-text--p c-button--archive c-button--form c-fw-700"><a href="<?php esc_url( home_url( '/' ) ) ; ?>">トップページへ</a></p>
+            <p class="c-text--p c-button--archive c-button--form c-fw-700">
+                <a href="<?php esc_url( home_url( '/' ) ) ; ?>">トップページへ</a>
+            </p>
         </div>
     </main>
 <?php get_sidebar(); ?>
